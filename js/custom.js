d852eb13280728cd57261e46980293cd2bf3bafd
Bolim Kim <lydia.kim.can@gmail.com>
2021-01-09 03:16:49 -0500

    Console issue clear

--- js/custom.js
+++ js/custom.js
@@ -83,26 +83,5 @@ $(window).load(function () {
         }
     });
 
-    // Carousel
-    $(document).ready(function(){
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav'
-          });
-          $('.slider-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            asNavFor: '.slider-for',
-            dots: true,
-            centerMode: true,
-            focusOnSelect: true
-          });
-      });
-
-
-
 
 });
